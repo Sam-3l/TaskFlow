e2584042ad53d91161f6432596c1ca0f055e75f7
Samuel <samuelonyeibor21@gmail.com>
2025-06-14 09:12:42 +0100

    refactor: remove AWS credentials from S3 client initialization for improved security

--- app/utils/s3_upload.py
+++ app/utils/s3_upload.py
@@ -4,8 +4,6 @@ import os
 def upload_file_to_s3(file, key, content_type='image/jpeg'):
     s3 = boto3.client(
         's3',
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
         region_name=os.getenv('S3_REGION', 'us-east-1')
     )
     
