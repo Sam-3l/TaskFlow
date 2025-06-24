cf0afd57d500b4a214f8662d45b271e0596d9854
Samuel <samuelonyeibor21@gmail.com>
2025-06-24 20:27:18 +0100

    refactor: update user name rendering to handle null last names across multiple templates

--- app/utils/oauth.py
+++ app/utils/oauth.py
@@ -15,7 +15,9 @@ def init_oauth(app):
         name='google',
         client_id=app.config['GOOGLE_CLIENT_ID'],
         client_secret=app.config['GOOGLE_CLIENT_SECRET'],
+        access_token_url='https://accounts.google.com/o/oauth2/token',
         access_token_params=None,
+        authorize_url='https://accounts.google.com/o/oauth2/auth',
         authorize_params=None,
         api_base_url='https://www.googleapis.com/oauth2/v1/',
         client_kwargs={
